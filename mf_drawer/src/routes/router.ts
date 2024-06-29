2789e64ae833b59c915d7d482aa21c570dd1f1a7
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-06-29 14:14:12 -0300

    feat(route): navigation mfdrawer structure

---
+++ mf_drawer/src/routes/router.ts
@@ -0,0 +1,73 @@
+enum Route {
+  Favorites = "favoritos",
+  Videos = "videos",
+}
+
+const Styles = {
+  width: "100%",
+  height: "100vh",
+  border: "none",
+};
+
+function createIframeHTML(
+  src: string,
+  styles: { width: string; height: string; border: string }
+): string {
+  return `<iframe src="${src}" style="width: ${styles.width}; height: ${styles.height}; border: ${styles.border};"></iframe>`;
+}
+
+const Router = {
+  setupNavigation: function () {
+    const videosLink = document.getElementById("videosLink");
+    const favoritesLink = document.getElementById("favoritesLink");
+
+    if (videosLink) {
+      videosLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        Router.loadRouteContent(Route.Videos);
+      });
+    } else {
+      console.error("Videos link not found");
+    }
+
+    if (favoritesLink) {
+      favoritesLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        Router.loadRouteContent(Route.Favorites);
+      });
+    } else {
+      console.error("Favorites link not found");
+    }
+  },
+
+  loadRouteContent: function (route: Route) {
+    console.log(`navigateTo function called with route: ${route}`);
+    const content = document.getElementById("content");
+
+    if (!content) {
+      console.error('Element with id "content" not found.');
+      return;
+    }
+
+    switch (route) {
+      case Route.Favorites:
+        content.innerHTML = createIframeHTML(
+          "http://localhost:3001/favoritos",
+          Styles
+        );
+        break;
+      case Route.Videos:
+        content.innerHTML = createIframeHTML("http://localhost:3001", Styles);
+        break;
+      default:
+        console.error(`Unknown route: ${route}`);
+    }
+  },
+
+  init: function () {
+    this.setupNavigation();
+    this.loadRouteContent(Route.Videos);
+  },
+};
+
+document.addEventListener("DOMContentLoaded", Router.init.bind(Router));
