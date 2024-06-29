340605e29c7f35b121e2f61cff12ed25fb654ca8
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-06-29 14:11:30 -0300

    feat: add koa server for microFronts

---
+++ mf_drawer/src/index.ts
@@ -0,0 +1,31 @@
+import Koa from "koa";
+import serve from "koa-static";
+import path from "path";
+import Router from "@koa/router";
+
+const app = new Koa();
+const router = new Router();
+const PORT = process.env.PORT || 3000;
+
+// Servindo arquivos estáticos
+app.use(serve(path.join(__dirname, "../static")));
+
+app.use(serve(path.join(__dirname, "../dist")));
+
+// Rotas
+router.get("/videos", (ctx: any) => {
+  ctx.type = "html";
+  ctx.body = path.join(__dirname, "../static", "index.html");
+});
+
+router.get("/favorites", (ctx: any) => {
+  ctx.type = "html";
+  ctx.body = path.join(__dirname, "../static", "index.html");
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+// Inicializando o servidor
+app.listen(PORT, () => {
+  console.log(`MF_DRAWER is running on http://localhost:${PORT}`);
+});
