ad87d12788c54a13e2a9a67f2cde855974b2d55b
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-06-30 10:39:41 -0300

    feat: Implement fetching and displaying favorite videos

---
+++ mf_video/src/favoritesScript.ts
@@ -0,0 +1,39 @@
+type VideoFavoriteItem = {
+  title: any;
+  id: { videoId: string };
+  snippet: { title: string };
+};
+
+async function fetchFavorites(): Promise<VideoFavoriteItem[]> {
+  const response = await fetch("http://localhost:3003/favorites");
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch favorites");
+  }
+  return data;
+}
+
+function showFavorites(favorites: VideoFavoriteItem[], container: HTMLElement) {
+  container.innerHTML = "";
+  favorites.forEach((video) => {
+    const videoElement = document.createElement("div");
+    videoElement.className = "video";
+    videoElement.innerHTML = `
+        <h3>${video.title}</h3>
+        <iframe src="https://www.youtube.com/embed/${video.id}" frameborder="0" allowfullscreen></iframe>
+      `;
+    container.appendChild(videoElement);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const favoritesContainer = document.getElementById(
+    "favoritesContainer"
+  ) as HTMLElement;
+  try {
+    const favorites = await fetchFavorites();
+    showFavorites(favorites, favoritesContainer);
+  } catch (error) {
+    console.error("Failed to load favorites:", error);
+  }
+});
